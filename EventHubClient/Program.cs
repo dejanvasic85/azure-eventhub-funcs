76aac4dd0c152e9098c7316afe13029484bd2c4a
Dejan Vasic <dejanvasic24@gmail.com>
2017-08-17 14:54:29 +1000

    Handling success and failure

---
+++ EventHubClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubClientCaller
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! Starting the event hub client");
+
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder("Endpoint=sb://modorevents.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=pAFwOYBHv75tU+nqxTxChw+T9BG/GxEfaawo7EeaFo0=")
+            {
+                EntityPath = "the-roof-is-on-fire"
+            };
+
+            var eventHubClient = Microsoft.Azure.EventHubs.EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(eventHubClient, 5);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an event hub client and sends 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(EventHubClient eventHubClient, int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var failOrSucceed = i % 2 == 0;
+                    var message = "{success : " + failOrSucceed + "}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
--- EventHubFailureHandler/index.js
+++ EventHubFailureHandler/index.js
@@ -1,8 +1,17 @@
 module.exports = function (context, eventHubMessages) {
     context.log(`JavaScript eventhub trigger function called for message array ${eventHubMessages}`);
-    
+
     eventHubMessages.forEach(message => {
+
         context.log(`Processed message ${message}`);
+
+        if (message.success === 'True') {
+            console.log("And it's a success!");
+            return Promise.resolve(true);
+        }
+
+        throw new Error('And its a Boom fail. :(');
+
     });
 
     context.done();
